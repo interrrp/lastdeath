67a30922884cb66b66e6a56f17ea87d31b22f35e
interrrp <watchyourcache@gmail.com>
2025-02-02 13:04:17 +0800

    refactor: use Fabric API instead of mixins

--- src/main/java/interrrp/lastdeath/LastDeathMod.java
+++ src/main/java/interrrp/lastdeath/LastDeathMod.java
@@ -2,10 +2,14 @@ package interrrp.lastdeath;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.mojang.brigadier.Command;
@@ -23,6 +27,8 @@ public final class LastDeathMod implements ModInitializer {
             LOGGER.warn("Failed to load last deaths", exc);
             LOGGER.warn("Previously stored death data was lost due to the above error");
         }
+
+        ServerLivingEntityEvents.AFTER_DEATH.register(this::onPlayerDeath);
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(CommandManager.literal("lastdeath").executes(context -> {
@@ -45,5 +51,22 @@ public final class LastDeathMod implements ModInitializer {
                 return Command.SINGLE_SUCCESS;
             }));
         });
+    }
+
+    private void onPlayerDeath(LivingEntity entity, DamageSource source) {
+        if (!(entity instanceof ServerPlayerEntity))
+            return;
+        var player = (ServerPlayerEntity) entity;
+
+        String name = player.getName().getString();
+        try {
+            LastDeathMod.STORAGE.setLastDeath(name, DeathInfo.fromPlayer(player));
+            Feedback.info(player, String.format("Your death at %s has been recorded.",
+                    player.getBlockPos().toShortString()));
+            Feedback.info(player, "Do /lastdeath to teleport to your death location.");
+        } catch (Exception exc) {
+            LastDeathMod.LOGGER.error("Failed to save death info", exc);
+            Feedback.error(player, "Failed to save death info");
+        }
     }
 }
